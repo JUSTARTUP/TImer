c6ae1b9dd139187fb868720911226404ecba1187
dodo07070707 <aa01056328583@gmail.com>
2024-06-02 17:05:47 +0900

    make template

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +32,126 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int seconds = 45 * 60;
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFFE7626C),
+        ),
+        width: screenWidth,
+        height: screenHeight,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: screenHeight / 844 * 50,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: screenWidth / 390 * 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.menu_rounded,
+                        color: Color(0xFFF4EDDB),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.refresh_sharp,
+                        color: Color(0xFFF4EDDB),
+                      ),
+                    ),
+                    SizedBox(
+                      width: screenWidth / 390 * 20,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: screenHeight / 844 * 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  '45', // 위에 변수 만들어 놨으니까 연산자 써서 자동화 ㄱㄱ
+                  style: TextStyle(
+                    fontSize: 90,
+                    color: Color(0xFFF4EDDB),
+                  ),
+                ),
+                SizedBox(width: screenWidth / 390 * 5),
+                const Text(
+                  ':',
+                  style: TextStyle(
+                    fontSize: 90,
+                    color: Color(0xFFF4EDDB),
+                  ),
+                ),
+                SizedBox(width: screenWidth / 390 * 5),
+                const Text(
+                  //여기도
+                  '00',
+                  style: TextStyle(
+                    fontSize: 90,
+                    color: Color(0xFFF4EDDB),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: screenHeight / 844 * 100),
+            const Icon(
+              //여기 삼항연산자 써서 저번에 했던거 처럼 아이콘 바꾸기, Icons.pause_circle_outline <- 이거 쓰면 됨
+              Icons.play_circle_outline,
+              color: Color(0xFFF4EDDB),
+              size: 100,
+            ),
+            SizedBox(height: screenHeight / 844 * 200),
+            Container(
+              height: screenHeight / 844 * 182.26,
+              width: screenWidth,
+              decoration: const BoxDecoration(
+                color: Color(0xFFF4EDDB),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: screenHeight / 844 * 40,
+                  ),
+                  const Text(
+                    'Pomodoros',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  SizedBox(height: screenHeight / 844 * 1),
+                  const Text(
+                    '0', //이거 변수(count)로 바꾸기
+                    style: TextStyle(color: Colors.black, fontSize: 60),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
